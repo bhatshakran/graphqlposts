6b7d1dd2acdcffac5b3325cc188664f3087bea94
Bhat Shaqran <bhatshakran@gmail.com>
2022-04-26 10:34:00 +0530

    Update comment mutation done

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -133,6 +133,19 @@ const Mutation = {
     return comment;
   },
 
+  updateComment(parent, args, { db }, info) {
+    const { id, data } = args;
+    const comment = db.comments.find((comment) => comment.id === id);
+    if (!comment) {
+      throw new Error("Comment not found!");
+    }
+    if (typeof data.text === "string") {
+      comment.text = data.text;
+    }
+
+    return comment;
+  },
+
   deleteComment(parent, args, { db }, info) {
     const commentIndex = db.comments.findIndex(
       (comment) => comment.id === args.id
