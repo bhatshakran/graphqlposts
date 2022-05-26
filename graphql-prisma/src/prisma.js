5a6f84f7d0aa13d49af9d71701e3be16211a4132
Bhat Shaqran <bhatshakran@gmail.com>
2022-05-26 18:03:04 +0530

    Mutation using async await done

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -41,17 +41,49 @@ const prisma = new Prisma({
 //   });
 
 
-prisma.mutation.updatePost({
-  where:{
-    id:"cl3mzc49l000c0a25m56oxp4b"
-  },
-  data: {
-    title:"Changed title",
-    body: "This is how to get started with GraphqQL",
-    published: true
-  }
-}, '{id}').then((data) => {
-  return prisma.query.posts(null, '{id title body published}')
-}).then((data)=>{
-console.log(data)
+// prisma.mutation.updatePost({
+//   where:{
+//     id:"cl3mzc49l000c0a25m56oxp4b"
+//   },
+//   data: {
+//     title:"Changed title",
+//     body: "This is how to get started with GraphqQL",
+//     published: true
+//   }
+// }, '{id}').then((data) => {
+//   return prisma.query.posts(null, '{id title body published}')
+// }).then((data)=>{
+// console.log(data)
+// })
+
+
+// using async await
+
+const createPostForUser = async(authorId, data) => {
+  const post = await prisma.mutation.createPost({
+    data:{
+      ...data,
+      author:{
+        connect:{
+          id: authorId
+        }
+      }
+    }
+  }, '{id}')
+
+  const user = await prisma.query.user({
+    where:{
+      id: authorId
+    }
+  }, '{id name email posts {id title published} }')
+  return user
+}
+
+
+createPostForUser('cl3mjxa8k001809255uf9jno1', {
+  title:"Great books to read",
+   body:"The art of War",
+   published: true
+}).then((user)=> {
+  console.log(JSON.stringify(user, undefined, 2))
 })
